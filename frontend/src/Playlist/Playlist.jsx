8c5ac321c14edd00dd25fb42144997d928e43eb2
Ved257 <ved.purohit25@gmail.com>
2022-09-12 16:41:09 +0530

    playlist_page

---
+++ frontend/src/Playlist/Playlist.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Playlist.css";
+import Search_icon from "./Search_icon.svg";
+
+const Playlist = () => {
+  return (
+    <div className="playlist">
+      <div className="searchbar">
+        <form>
+          <label>
+            <img src={Search_icon} />
+          </label>
+          <input className="search" type="text" />
+        </form>
+      </div>
+      <div className="tables">
+        <table className="play_lists">
+          <tr className="heading">
+            <th>Serial Number</th>
+            <th>Playlist Name</th>
+            <th>Author</th>
+            <th>Date Added</th>
+          </tr>
+          <tr className="contents">
+            <td>Serial Number</td>
+            <td>Playlist Name</td>
+            <td>Autdor</td>
+            <td>Date Added</td>
+          </tr>
+          <br/>
+          <tr className="contents">
+            <td>Serial Number</td>
+            <td>Playlist Name</td>
+            <td>Autdor</td>
+            <td>Date Added</td>
+          </tr>
+          <br/>
+          <tr className="contents">
+            <td>Serial Number</td>
+            <td>Playlist Name</td>
+            <td>Autdor</td>
+            <td>Date Added</td>
+          </tr>
+          <br/>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Playlist;
