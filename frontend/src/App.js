3203bfe6eae5c13fe53f89fccc7903c859106294
shashank-suresh <shashanksuresh3@gmail.com>
2022-09-13 13:27:16 +0530

    Added global navigation between components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,19 @@ import Home from "./Home/Home"
 import Song from "./Song/Song"
 import Playlist from './Playlist/Playlist';
 import UserHome from './UserHome/UserHome';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 function App() {
   return (
     <>
-      <Home />
-      <Song />
-      <Playlist />
-      <UserHome />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/song' element={<Song />} />
+          <Route path='/playlists' element={<Playlist />} />
+          <Route path='/userhome' element={<UserHome />} />
+        </Routes>
+      </Router>
     </>
   );
 }
